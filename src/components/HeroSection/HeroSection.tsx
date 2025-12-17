94bfeec76676475ecf7a01e993b069f626d42da1
Alex Sander <alex.darienko@gmail.com>
2025-12-17 19:56:29 +0200

    Hero done

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -1,12 +1,40 @@
-import styles from './HeroSection.module.scss';
+import styles from "./HeroSection.module.scss";
 
 const HeroSection = () => {
   return (
-    <div className={styles.wrap}>
+    <section className={styles.wrap}>
       <div className={styles.inner}>
 
+        {/* content */}
+        <div className={styles.content}>
+          <h1 className={styles.title}>
+            IMPROVE YOUR <br />
+            CULINARY TALENTS
+          </h1>
+
+          <p className={styles.text}>
+            Amazing recipes for beginners in the world of cooking, enveloping
+            you in the aromas and tastes of various cuisines.
+          </p>
+
+          <button className={styles.button}>ADD RECIPE</button>
+        </div>
+
+        {/* images */}
+        <div className={styles.images}>
+          <img
+            src="/images/hero-small-image.png"
+            alt="Dessert"
+            className={styles.imageSmall}
+          />
+          <img
+            src="/images/hero-large-image.png"
+            alt="Beef"
+            className={styles.imageLarge}
+          />
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
