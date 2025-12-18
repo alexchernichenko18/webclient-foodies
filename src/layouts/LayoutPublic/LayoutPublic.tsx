7d080b9255fba7ff1ee80a9b8484ca17fc43724f
Alex Sander <alex.darienko@gmail.com>
2025-12-18 17:16:52 +0200

    Testimonials done

--- src/layouts/LayoutPublic/LayoutPublic.tsx
+++ src/layouts/LayoutPublic/LayoutPublic.tsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import HeroSection from "../../components/HeroSection/HeroSection";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
+import Testimonials from "../../components/Testimonials/Testimonials";
 
 import styles from "./LayoutPublic.module.scss";
 
@@ -11,6 +12,7 @@ const LayoutPublic = () => {
       <Header variant="dark" />
       <HeroSection />
       <Outlet />
+      <Testimonials />
       <Footer />
     </div>
   );
